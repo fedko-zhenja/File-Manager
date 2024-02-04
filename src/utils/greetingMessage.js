cfd9174cc5e872028e6df666d5cb69d86b02364c
@fedko-zhenja <fedkozhenja@gmail.com>
2024-02-04 22:52:39 +0400

    reafctor: update greetingMessage

--- src/utils/greetingMessage.js
+++ src/utils/greetingMessage.js
@@ -1,7 +1,8 @@
 import { messages } from "../data/messages.js";
 import { createCurrentDirMessage } from "./currentDirMessage.js";
+import { homedir } from 'node:os';
 
 export function createGreetingMessage (userName) {
     console.log('\x1b[32m%s\x1b[0m', `${messages.greeting}, ${userName}!`);
-    createCurrentDirMessage();
+    createCurrentDirMessage(homedir());
 }
